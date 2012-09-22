c56e733abfe3015069aaab7ca47611d65ffaa85e
Van Gysegem Thomas <thomasvangysegem@gmail.com>
2012-09-22 23:55:00 +0200

    -Diverses retouche -Finalisation de la modification de palette -Modification du dossier etape 1 (manque plus que le schema des processus)

--- Traitement d'image/src/Picture.py
+++ Traitement d'image/src/Picture.py
@@ -12,26 +12,48 @@ class Picture():
     def __init__(self):
         self.image = 0
         self.data = []                  # Couleur de chaque pixel
+        self.palette = None             # Palette de couleurs utilisée pour les images indexées
     
     # Ouvre une image
     def open(self, filename):
         print "Ouverture de l'image ..."
         self.setImage(Image.open(filename))
+        
+        print "Mode :", self.image.mode
     
     # Donne une image
     def setImage(self, image):
         self.image = image
         self.data = list(self.image.getdata())
-        
-        print "Mode :", self.image.mode
     
     # Modifie une couleur de la palette
-    def setPaletteColor(self, oldColor, newColor):
+    def setPaletteColor(self, pos, newColor):
+        # Vérifie les absices
+        if pos[0] < 0:
+            pos[0] = 0
+        elif pos[0] >= self.image.size[0]:
+            pos[0] = self.image.size[0] - 1
+        
+        # Verifie les ordonnées
+        if pos[1] < 0:
+            pos[1] = 0
+        elif pos[1] >= self.image.size[1]:
+            pos[1] = self.image.size[1] - 1
+        
         # Niveau de gris
         if self.image.mode == "L":
+            # Vérification des paramétres
             if type(newColor) != type(0):
                 print "Erreur: Color doit être en niveau de gris !", newColor
                 return
+            
+            if newColor < 0:
+                newColor = 0
+            elif newColor > 255:
+                newColor = 255
+            
+            # Récupére l'ancienne couleur
+            oldColor = self.image.getpixel(pos)
             
             # Remplace la couleur
             for index, value in enumerate(self.data):
@@ -40,6 +62,32 @@ class Picture():
         
             # Recrée l'image
             self.image.putdata(self.data)
+        # Couleurs indexée
+        elif self.image.mode == "P":
+            # Vérification des paramétres
+            if type(newColor) != type((0,0)):
+                print "Erreur: Color doit être en RGB !", newColor
+                return
+            
+            for index, value in enumerate(newColor):
+                if value < 0:
+                    newColor[index] = 0
+                elif value > 255:
+                    newColor[index] = 255
+            
+            # Récupére la palette
+            if self.palette == None:
+                self.getPalette()
+            
+            # Récupére la position de la couleur
+            index = self.image.getpixel(pos) * 3
+            
+            self.palette[index] = newColor[0]
+            self.palette[index + 1] = newColor[1]
+            self.palette[index + 2] = newColor[2]
+            
+            #? Enregistre les modifications
+            self.image.putpalette(self.palette)
     
     # Modifie la taille de l'image
     def setSize(self, w, h):
@@ -66,6 +114,7 @@ class Picture():
         # Création de la nouvelle image
         image = Image.new(self.image.mode, (w, h))
         image.putdata(data)
+        image.putpalette(self.getPalette())
         self.setImage(image)
     
     # Donne une image Qt depuis une image PIL
@@ -91,6 +140,18 @@ class Picture():
     def getPixel(self, x, y):
         if self.image == 0:
             return (0, 0, 0)
+        
+        # Couleurs indexée
+        if self.image.mode == "P":
+            if self.palette == None:
+                self.getPalette()
+            
+            index = self.image.getpixel((x, y)) * 3
+            r = self.palette[index]
+            g = self.palette[index + 1]
+            b = self.palette[index + 2]
+            
+            return (r, g, b)
         
         return self.image.getpixel((x, y))
         
@@ -135,3 +196,13 @@ class Picture():
         roi.putdata(data)
         
         return roi
+    
+    # Génére la palette si elle n'existe pas
+    def getPalette(self):
+        if self.image.mode != "P":
+            print "Aucune palette disponnible pour ce mode"
+            return None
+        
+        self.palette = self.image.getpalette()
+        return self.palette
+        
--- Traitement d'image/src/dialog/DialogModifPalette.py
+++ Traitement d'image/src/dialog/DialogModifPalette.py
@@ -16,7 +16,28 @@ class DialogModifPalette(QtGui.QDialog):
         self.ui.setupUi(self)
         # Fin du wrapper
         
+        self.ui.inputRed.setMinimum(0)
+        self.ui.inputGreen.setMinimum(0)
+        self.ui.inputBlue.setMinimum(0)
+        self.ui.inputGray.setMinimum(0)
+        
+        self.ui.inputRed.setMaximum(255)
+        self.ui.inputGreen.setMaximum(255)
+        self.ui.inputBlue.setMaximum(255)
+        self.ui.inputGray.setMaximum(255)
+            
+        self.ui.inputX.setMinimum(0)
+        self.ui.inputY.setMinimum(0)
+        
         self.parent = parent
+        
+        # Evenements
+        self.ui.inputRed.valueChanged.connect(self.majColor)
+        self.ui.inputGreen.valueChanged.connect(self.majColor)
+        self.ui.inputBlue.valueChanged.connect(self.majColor)
+        self.ui.inputGray.valueChanged.connect(self.majColor)
+        self.ui.inputX.valueChanged.connect(self.majColor)
+        self.ui.inputY.valueChanged.connect(self.majColor)
     
     # Ferme le dialogue
     def close(self):
@@ -34,30 +55,65 @@ class DialogModifPalette(QtGui.QDialog):
             print "Operation interdite sans image"
             return
         
-        if self.parent.basePicture.image.mode != "L":
+        if self.parent.basePicture.image.mode != "L" and self.parent.basePicture.image.mode != "P":
             print "Operation interdite sur une image couleur"
             return
         
+        self.ui.inputX.setMaximum(self.parent.basePicture.image.size[0])
+        self.ui.inputY.setMaximum(self.parent.basePicture.image.size[1])
+        
         self.parent.setColorPickerMode()
         super(DialogModifPalette, self).show()
+    
+    # Met a jour les couleurs affichées
+    def majColor(self, value):
+        # Récupére les saisies
+        r = self.ui.inputRed.value()
+        g = self.ui.inputGreen.value()
+        b = self.ui.inputBlue.value()
+        
+        gray = self.ui.inputGray.value()
+        
+        x = self.ui.inputX.value()
+        y = self.ui.inputY.value()
+        
+        # Récupére l'ancienne couleur
+        oldColor = self.parent.basePicture.getPixel(x, y)
+        
+        # Crée les pixmap
+        oldColorPixmap = QtGui.QPixmap(25, 25)
+        newColorPixmap = QtGui.QPixmap(25, 25)
+        
+        # Niveau de gris
+        if self.parent.basePicture.image.mode == "L":
+            oldColorPixmap.fill(QtGui.QColor(oldColor, oldColor, oldColor))
+            newColorPixmap.fill(QtGui.QColor(gray, gray, gray))
+        # Couleur
+        else:
+            oldColorPixmap.fill(QtGui.QColor(oldColor[0], oldColor[1], oldColor[2]))
+            newColorPixmap.fill(QtGui.QColor(r, g, b))
+        
+        # Met a jout l'image
+        self.ui.oldColor.setPixmap(oldColorPixmap)
+        self.ui.newColor.setPixmap(newColorPixmap)
     
     # Récupére une couleur et sa position
     def setPickedColor(self, pos, color):
         # Niveau de gris
         if type(color) == type(0):
-            self.ui.inputRed.setText("0")
-            self.ui.inputGreen.setText("0")
-            self.ui.inputBlue.setText("0")
-            self.ui.inputGray.setText("%d" % color)
+            self.ui.inputRed.setValue(0)
+            self.ui.inputGreen.setValue(0)
+            self.ui.inputBlue.setValue(0)
+            self.ui.inputGray.setValue(color)
         # Indexées
         else:
-            self.ui.inputRed.setText("%d" % color[0])
-            self.ui.inputGreen.setText("%d" % color[1])
-            self.ui.inputBlue.setText("%d" % color[2])
-            self.ui.inputGray.setText("0")
+            self.ui.inputRed.setValue(color[0])
+            self.ui.inputGreen.setValue(color[1])
+            self.ui.inputBlue.setValue(color[2])
+            self.ui.inputGray.setValue(0)
         
-        self.ui.inputX.setText("%d" % pos[0])
-        self.ui.inputY.setText("%d" % pos[1])
+        self.ui.inputX.setValue(pos[0])
+        self.ui.inputY.setValue(pos[1])
     
     # Rejet des modifications
     def reject(self):
@@ -70,33 +126,23 @@ class DialogModifPalette(QtGui.QDialog):
             return
         
         # Récupére la couleur
-        try:
-            r = int(self.ui.inputRed.text())
-            g = int(self.ui.inputGreen.text())
-            b = int(self.ui.inputBlue.text())
-            
-            gray = int(self.ui.inputGray.text())
-            
-            x = int(self.ui.inputX.text())
-            y = int(self.ui.inputY.text())
-        except ValueError:
-            r = 0
-            g = 0
-            b = 0
-            
-            gray = 0
-            
-            x = 0
-            y = 0
+        r = self.ui.inputRed.value()
+        g = self.ui.inputGreen.value()
+        b = self.ui.inputBlue.value()
         
-        oldColor = self.parent.basePicture.getPixel(x, y)
+        gray = self.ui.inputGray.value()
+        
+        x = self.ui.inputX.value()
+        y = self.ui.inputY.value()
+        
+        pos = (x, y)
         if self.parent.basePicture.image.mode == "L":
             color = (gray)
         else:
             color = (r, g, b)
         
         self.parent.resultPicture.setImage(self.parent.basePicture.image.copy())
-        self.parent.resultPicture.setPaletteColor(oldColor, color)
+        self.parent.resultPicture.setPaletteColor(pos, color)
         self.parent.showResultPicture()
         
         self.close()
