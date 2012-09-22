abf8ad4dd2dfd5e5de0471e2a2b7cdd617aaf804
Van Gysegem Thomas <thomasvangysegem@gmail.com>
2012-09-23 01:36:32 +0200

    -Finalisation de l'etape 1: dossier fini

---
+++ Traitement d'image/src/layout/pyuic4.bat
@@ -0,0 +1,3 @@
+@"C:\Python27\python" "C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py" windowsLayout.ui -o windowsLayout.py
+@"C:\Python27\python" "C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py" dialogModifTaille.ui -o dialogModifTaille.py
+@"C:\Python27\python" "C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py" dialogModifPalette.ui -o dialogModifPalette.py
